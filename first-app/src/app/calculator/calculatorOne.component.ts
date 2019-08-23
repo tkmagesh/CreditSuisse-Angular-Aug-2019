316810bf25297479ebf8f0e52478660e667a79a2
Magesh Kuppan <tkmagesh77@gmail.com>
2019-08-23 14:09:11 +0530

    using ngClass directive

--- first-app/src/app/calculator/calculatorOne.component.ts
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -3,7 +3,8 @@ import { CalculatorModel } from './CalculatorModel';
 
 @Component({
 	selector : 'app-calculator-1',
-	templateUrl : 'calculatorOne.component.html'
+	templateUrl : 'calculatorOne.component.html',
+	styleUrls : ['calculatorOne.component.css']
 })
 export class CalculatorOneComponent{
 	
