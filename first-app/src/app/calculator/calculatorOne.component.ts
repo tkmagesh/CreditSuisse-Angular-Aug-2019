cdc997b85307907dcf4702908ae78c2fc58c535e
Magesh Kuppan <tkmagesh77@gmail.com>
2019-08-23 11:37:36 +0530

    using the change event for the text boxes

--- first-app/src/app/calculator/calculatorOne.component.ts
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -6,16 +6,26 @@ import { Component } from '@angular/core';
 })
 export class CalculatorOneComponent{
 	result : number = 0;
+	value1 : number = 0;
+	value2 : number = 0;
+
+	setValue1(v){
+		this.value1 = Number(v);
+	}
+
+	setValue2(v){
+		this.value2 = Number(v);
+	}
 	onAddClick(value1, value2){
-		this.result = Number(value1) + Number(value2);
+		this.result = this.value1 + this.value2;
 	}
 	onSubtractClick(value1, value2){
-		this.result = Number(value1) - Number(value2);
+		this.result = this.value1 - this.value2;
 	}
 	onMultiplyClick(value1, value2){
-		this.result = Number(value1) * Number(value2);
+		this.result = this.value1 * this.value2;
 	}
 	onDivideClick(value1, value2){
-		this.result = Number(value1) / Number(value2);
+		this.result = this.value1 / this.value2;
 	}
 }
