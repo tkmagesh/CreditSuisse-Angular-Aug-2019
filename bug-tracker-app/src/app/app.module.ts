49a7afd43b2655db679220e53cdf45dc14c02f50
Magesh Kuppan <tkmagesh77@gmail.com>
2019-08-24 15:53:06 +0530

    refactoring to utilsModule

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UtilsModule } from './utils/utils.module';
 
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 import { BugStatsComponent } from './bugTracker/views/bugStats.component';
-
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { SortPipe } from './bugTracker/pipes/sort.pipe';
-import { ElapsedPipe } from './bugTracker/pipes/elapsed.pipe';
 
 import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
 
@@ -22,14 +19,12 @@ import { BugStorageService } from './bugTracker/services/bugStorage.service';
     AppComponent,
     BugTrackerComponent,
     BugStatsComponent,
-    TrimTextPipe,
-    SortPipe,
-    ElapsedPipe,
     ClosedCountPipe
   ],
   imports: [
     BrowserModule
     , FormsModule
+    , UtilsModule
   ],
   providers: [
   	BugOperationsService
---
+++ bug-tracker-app/src/app/utils/utils.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { TrimTextPipe } from './pipes/trimText.pipe';
+import { SortPipe } from './pipes/sort.pipe';
+import { ElapsedPipe } from './pipes/elapsed.pipe';
+
+@NgModule({
+	declarations : [
+		TrimTextPipe,
+	    SortPipe,
+	    ElapsedPipe
+	],
+	providers : [],
+	imports : [],
+	bootstrap : [],
+	exports : [
+		TrimTextPipe,
+	    SortPipe,
+	    ElapsedPipe
+	]
+})
+export class UtilsModule{}
